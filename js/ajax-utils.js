fa3c068d13b5935bf17bb04d6341b805521530cb
diegodesouza <diegotrus7@gmail.com>
2016-03-13 20:18:14 -0400

    .

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -3,37 +3,34 @@
 // Set up a namespace for our utility
 var ajaxUtils = {};
 
-
 // Returns an HTTP request object
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
-  } 
+  }
   else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
+  }
   else {
     global.alert("Ajax is not supported!");
-    return(null); 
+    return(null);
   }
 }
 
-
 // Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
+ajaxUtils.sendGetRequest =
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
+    request.onreadystatechange =
+      function() {
+        handleResponse(request,
                        responseHandler,
-                       isJsonResponse); 
+                       isJsonResponse);
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
   };
-
 
 // Only calls user provided 'responseHandler'
 // function if response is ready
@@ -58,10 +55,7 @@ function handleResponse(request,
   }
 }
 
-
 // Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
 
-
 })(window);
-
